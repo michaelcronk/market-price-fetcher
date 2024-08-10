137163f60e5fa1ec2e013f0eadbc29a63ac28ccd
Michael Cronk <michael.cronk1@gmail.com>
2024-08-10 06:37:40 -0700

    updating retrieve data to flask

--- retrieve_data.py
+++ retrieve_data.py
@@ -1,16 +1,24 @@
+from flask import Flask, render_template, request
 import datetime
 import requests
-import creds
+import creds  # Your API key should be stored here
 
-current_date = datetime.date.today()
-previous_day = current_date - datetime.timedelta(days = 1)
-ticker = input("What ticker would you like to look up?\n").upper()
+app = Flask(__name__)
 
-spy_url = f"https://api.polygon.io/v1/open-close/{ticker}/{previous_day}?adjusted=true&apiKey={creds.api_key}"
-response = requests.get(spy_url)
-data = response.json()
-print(f"Ticker: {data["symbol"]}")
-print(f"Date: {data["from"]}")
-print(f"Market open price: ${data["open"]}")
-print(f"Market close price: ${data["close"]}")
 
+@app.route("/", methods=["GET", "POST"])
+def stock_lookup():
+    data = None
+    if request.method == "POST":
+        ticker = request.form.get("ticker").upper()
+        current_date = datetime.date.today()
+        previous_day = current_date - datetime.timedelta(days=1)
+        spy_url = f"https://api.polygon.io/v1/open-close/{ticker}/{previous_day}?adjusted=true&apiKey={creds.api_key}"
+        response = requests.get(spy_url)
+        data = response.json()
+
+    return render_template("index.html", data=data)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
