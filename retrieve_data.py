b5aef429e48c5b22c208f917d9884417c804f212
Michael Cronk <michael.cronk1@gmail.com>
2024-08-10 06:49:01 -0700

    created main html page

--- retrieve_data.py
+++ retrieve_data.py
@@ -6,14 +6,11 @@ import creds  # Your API key should be stored here or in .env
 app = Flask(__name__)
 
 
-# Main flask route
 @app.route("/", methods=["GET", "POST"])
 def stock_lookup():
     data = None
     if request.method == "POST":
-        ticker = request.form.get(
-            "ticker"
-        ).upper()  # Making sure the ticker is uppercase
+        ticker = request.form.get("ticker").upper()  # Making the ticker uppercase
         current_date = datetime.date.today()
         previous_day = current_date - datetime.timedelta(days=1)
         url = f"https://api.polygon.io/v1/open-close/{ticker}/{previous_day}?adjusted=true&apiKey={creds.api_key}"
